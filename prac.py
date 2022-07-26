aceeb126440ac5d13d3c7e2ef7f7caa6395ef5e1
Karl M <46508128+wwwhatgit@users.noreply.github.com>
2022-07-25 19:01:33 -0700

    init change

---
+++ prac.py
@@ -0,0 +1,36 @@
+# Valid Anagram leet code 242
+from re import S
+
+
+class Solution:
+    def __init__(self, s1, t1):
+       self.str = s1
+       self.tr = t1
+    def isAnagram(self):
+        s = self.str
+        t = self.tr
+        if len(s) != len(t):
+            return False
+
+        countStr={}
+        for i in range(len(s)):
+            countStr[s[i]] = 1+countStr.get(s[i],0)   
+        for i in range(len(t)):
+            countStr[t[i]] = countStr.get(t[i],0)-1
+
+        for key,value in countStr.items():
+            if value!=0:
+                return False
+        return True
+    
+
+        
+
+sr = 'anagram'
+tr = 'nagaram'
+
+sol = Solution(sr,tr)
+sol.isAnagram()
+import git
+my_repo = git.Repo('https://github.com/wwwhatgit/python_prac.git')
+git.Repo.clone_from('https://github.com/wwwhatgit/python_prac.git', 'python_prac-https')
